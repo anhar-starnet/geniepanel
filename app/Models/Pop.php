4c43655ee20dac4e5df2350729b162b2a705ce61
STARNET <admin@starnet.local>
2026-08-03 02:18:55 +0000

    Refactor Pop model

--- app/Models/Pop.php
+++ app/Models/Pop.php
@@ -9,46 +9,33 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class Pop extends Model
 {
     protected $fillable = [
-
         'area_id',
-
         'code',
-
         'name',
-
-        'mikrotik_name',
-
-        'olt_name',
-
-        'ip_address',
-
         'address',
-
         'latitude',
-
         'longitude',
-
         'description',
-
         'status',
-
     ];
 
     protected $casts = [
-
-        'status'=>'boolean',
-
-        'latitude'=>'decimal:7',
-
-        'longitude'=>'decimal:7',
-
+        'status' => 'boolean',
+        'latitude' => 'decimal:7',
+        'longitude' => 'decimal:7',
     ];
 
+    /**
+     * POP belongs to Area
+     */
     public function area(): BelongsTo
     {
         return $this->belongsTo(Area::class);
     }
 
+    /**
+     * POP has many ODP
+     */
     public function odps(): HasMany
     {
         return $this->hasMany(Odp::class);
