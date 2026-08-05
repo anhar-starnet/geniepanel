e003bd81d539430aeaead80f81707b864a1a6233
STARNET <admin@starnet.local>
2026-08-05 12:59:06 +0000

    feat(ui): improve adminlte branding and navigation

--- config/adminlte.php
+++ config/adminlte.php
@@ -14,7 +14,7 @@ return [
     |
     */
 
-    'title' => 'AdminLTE 3',
+    'title' => 'GeniePanel',
     'title_prefix' => '',
     'title_postfix' => '',
 
@@ -63,7 +63,7 @@ return [
     |
     */
 
-    'logo' => '<b>Admin</b>LTE',
+    'logo' => '<b>Genie</b>Panel',
     'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
@@ -257,7 +257,7 @@ return [
     */
 
     'use_route_url' => false,
-    'dashboard_url' => 'home',
+    'dashboard_url' => 'dashboard',
     'logout_url' => 'logout',
     'login_url' => 'login',
     'register_url' => 'register',
@@ -373,8 +373,8 @@ return [
     ],
 
     [
-        'text' => 'GenieACS',
-        'url'  => '#',
+        'text' => 'GenieACS Devices',
+        'url'  => 'genieacs/devices',
         'icon' => 'fas fa-server',
     ],
 
@@ -406,7 +406,7 @@ return [
 
     [
         'text' => 'Dashboard NOC',
-        'url'  => '#',
+        'url'  => 'dashboard',
         'icon' => 'fas fa-chart-line',
     ],
 
